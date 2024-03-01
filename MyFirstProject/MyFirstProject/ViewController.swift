348d0b1fd3b508e815a0aba0acb6f31778e43a12
jinsuwee <amglt2@naver.com>
2024-03-01 15:20:15 +0900

    1주차 실습

---
+++ MyFirstProject/MyFirstProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyFirstProject
+//
+//  Created by Jinsu Wee on 3/1/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
